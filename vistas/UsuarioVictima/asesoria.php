e4a111c13f25fd4ccfebcaf2be825c5a22bbbac3
Fiorela Mamani <fiorela.12arq@gmail.com>
2021-09-12 20:36:41 -0500

    Interfaz de Asesoría y modificaciones en b_panico

---
+++ vistas/UsuarioVictima/asesoria.php
@@ -0,0 +1,252 @@
+<link type="text/css" rel="stylesheet" href="_recursos/input-file/css/disenio_input.css">
+
+<div class="contendor_kn">
+    <div class="panel panel-default">
+        <div class="panel-heading">
+            <center><h3><b>ASESORÍA</b></h3></center>
+            <center><label>¿Qué tipo de Asesoría Necesita?</label></center>
+        </div> 
+    </div>
+</div>
+<div class="contendor_kn">
+    <div class="panel panel-default"> 
+      <div class="panel-body">
+        <label >Legal</label> 
+            <div class="col-md-12 col-lg-12 col-xs-12" style="text-align:center;">
+                <div class="col-md-12">
+                <br><button class="btn btn-success" onclick="" data-toggle="modal" data-target="#Modal"><strong > Proceso de Alimentos</strong></button>
+                </div>
+            </div>   
+                    
+            <div class="col-md-12 col-lg-12 col-xs-12" style="text-align:center;">
+                <div class="col-md-12">
+                <br><button class="btn btn-success" onclick="" data-toggle="modal" data-target="#Modal2"><strong> Tenencia de Menores</strong></button>
+                </div>
+            </div>
+    </div>
+</div>
+
+    <div class="panel panel-default"> 
+      <div class="panel-body"> 
+        <label >Psicológica</label> 
+            <div class="col-md-12 col-lg-12 col-xs-12" style="text-align:center;">
+                <div class="col-md-12">
+                <br><button class="btn btn-success" onclick="" data-toggle="modal" data-target="#Modal3"><strong> Crisis Emocional</strong></button>
+                </div>
+            </div>   
+                    
+            <div class="col-md-12 col-lg-12 col-xs-12" style="text-align:center;">
+                <div class="col-md-12">
+                <br><button class="btn btn-success" onclick="" data-toggle="modal" data-target="#Modal4"><strong> Violación Sexual</strong></button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="">
+        <div style="text-align: center;" align="center">
+            <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/ayuda.png">
+        </div> 
+    </div>
+      
+<!-- Modal1 -->
+<div class="modal fade" id="Modal" tabindex="-1" role="dialog" aria-labelledby="ModalLabel" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="panel panel-default" id="ModalLabel"><center><b>Proceso de Alimentos</b></center></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div><br>
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div> 
+              </div>         
+            </div>
+      </div>
+      <div class="modal-footer">
+        <div class="panel panel-default">
+            <center><label>¿Resolvimos su duda?</label></center></div>
+        <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#Modal5">No</button>
+        <button type="button" class="btn btn-info" data-dismiss="modal">Si</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal2 -->
+<div class="modal fade" id="Modal2" tabindex="-1" role="dialog" aria-labelledby="ModalLabel1" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="panel panel-default" id="ModalLabel1"><center><b>Tenencia de Menores</b></center></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div><br>
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div> 
+              </div>         
+            </div>
+      </div>
+      <div class="modal-footer">
+        <div class="panel panel-default">
+            <center><label>¿Resolvimos su duda?</label></center></div>
+        <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#Modal5">No</button>
+        <button type="button" class="btn btn-info" data-dismiss="modal">Si</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal3 -->
+<div class="modal fade" id="Modal3" tabindex="-1" role="dialog" aria-labelledby="ModalLabel3" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="panel panel-default" id="ModalLabel3"><center><b>Crisis Emocional</b></center></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div><br>
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div> 
+              </div>         
+            </div>
+      </div>
+      <div class="modal-footer">
+        <div class="panel panel-default">
+            <center><label>¿Resolvimos su duda?</label></center></div>
+        <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#Modal5">No</button>
+        <button type="button" class="btn btn-info" data-dismiss="modal">Si</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- Modal4 -->
+<div class="modal fade" id="Modal4" tabindex="-1" role="dialog" aria-labelledby="ModalLabel4" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="panel panel-default" id="ModalLabel4"><center><b>Violación sexual</b></center></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div><br>
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/afiche.jpg">
+                </div> 
+              </div>         
+            </div>
+      </div>
+      <div class="modal-footer">
+        <div class="panel panel-default">
+            <center><label>¿Resolvimos su duda?</label></center></div>
+        <button type="button" class="btn btn-danger"  data-toggle="modal" data-target="#Modal5">No</button>
+        <button type="button" class="btn btn-info" data-dismiss="modal">Si</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+<!-- en caso seleccione no -->
+<div class="modal fade" id="Modal5" tabindex="-1" role="dialog" aria-labelledby="ModalLabel5" aria-hidden="true">
+  <div class="modal-dialog" role="document">
+    <div class="modal-content">
+      <div class="modal-header">
+        <h4 class="panel panel-default" id="ModalLabel5"><center><b>Asesoría Gratuita</b></center></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">  
+        <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
+            <div class="panel panel-default">
+              <div class="panel-body">
+              <div class="col-md-12 col-lg-12 col-xs-12"> 
+                <div class="col-sm-12">
+                    <label>Número de Celular</label>
+                    <input id=""  type="text" style="background-color: #FFFFFF"  placeholder="Ingrese nro de celular" class="form-control" >
+                    <br>
+                </div><br>
+                <div style="text-align: center;" align="center">
+                    <img class="imagen" style="text-align: center;" align="center" src="_recursos/img/llamada.png">
+                </div> 
+              </div>         
+            </div>
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-danger" data-dismiss="modal">Cancelar</button>
+        <button type="button" class="btn btn-info">Entendido</button>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+
+    
+
+
+<script type="text/javascript">
+</script>
+<script type="text/javascript" src="_recursos/js/consola_victima.js"></script>
+
+<style type="text/css">
+  .contendor_kn{
+    padding: 10px;
+  }
+</style>
+<style type="text/css">
+    label{
+      font-weight:bold;
+    }
+    .select2{
+      font-weight:bold;
+      text-align-last:center;
+    }
+    button{
+    font-weight:bold;
+    
+    }
+    select{
+       font-weight:bold;
+      text-align-last:center;
+    }
+    .select2-container--default.select2-container--disabled .select2-selection--single{
+      color: rgb(25,25,51); background-color: rgb(255,255,255);solid 5px;
+    }
+    .modal-open .select2-container--open {
+      z-index: 999999 !important; width:100% !important; 
+    }
+</style>
+
--- vistas/UsuarioVictima/bpanico.php
+++ vistas/UsuarioVictima/bpanico.php
@@ -7,7 +7,7 @@
       </div>
       <div class="panel-body">   
         <div style="text-align: center;" align="center">
-          <img class="imagen0" style="text-align: center;" onclick="abrirmodaladministrativo()" align="center" src="_recursos/img/botonp.png"> 
+          <img class="imagen0" style="text-align: center;" onclick="" data-toggle="modal" data-target="#Modal0"  align="center" src="_recursos/img/botonp.png"> 
         </div> 
       </div>
     </div>
@@ -19,15 +19,18 @@
     </div>
 </div>
 
-<div class="modal fade bs-example-modal-lg" id="modal_editar_adminsitrador" >
-  <div class="modal-dialog modal-lg">
+
+<!-- Mensaje -->
+<div class="modal fade" id="Modal0" tabindex="-1" role="dialog" aria-labelledby="ModalLabel0" aria-hidden="true">
+  <div class="modal-dialog" role="document">
     <div class="modal-content">
-         <div class="modal-header">
-           <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-           <h4 class="modal-title" id="myModalLabel"><label> </label></h4>
-         </div>
-        <div class="modal-body">
-          <div class="contendor_kn">
+      <div class="modal-header">
+        <h4 class="modal-title" id="ModalLabel0"><center><b></b></center></h4>
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+          <span aria-hidden="true">&times;</span>
+        </button>
+      </div>
+      <div class="modal-body">
             <div class="panel panel-default">
               <div class="panel-body">
               <div style="text-align: center;" align="center">
@@ -35,13 +38,12 @@
               </div> 
               </div>         
             </div>
-          </div>  
-        </div> 
-        <div class="modal-footer">
-            <button type="button" class="btn btn-info pull-right" data-dismiss="modal"></i><strong> Entendido</strong></button>
-        </div> 
+      </div>
+      <div class="modal-footer">
+        <button type="button" class="btn btn-info" data-dismiss="modal">Entendido</button>
+      </div>
     </div>
-  </div> 
+  </div>
 </div>
 
 <script type="text/javascript">
@@ -76,38 +78,4 @@
       z-index: 999999 !important; width:100% !important; 
     }
 </style>
-<script type="text/javascript">
-  traer_administrador();
-    $(document).on('submit', '#update-form-administrador', function() { 
-      var data = $(this).serialize(); 
-      $.ajax({  
-        type : 'POST',
-        mimeType: "multipart/form-data",
-        url  : '',
-        data:  new FormData(this),
-        contentType: false,
-        cache: false,
-        processData:false,
-        success:function(resp) {
-          $("#modal_editar_adminsitrador").modal('hide');
-          traer_administrador();
-          document.getElementById("update-form-administrador").reset();
-          if(resp>0){
-            swal("Datos Actualizados","","success")
-            .then ( ( value ) =>  { 
-              document.getElementById("update-form-administrador").reset();
-              traer_administrador();
-            } ) ;
-          }else{
-            swal("Lo sentimos los datos no fueron registrados","","error")
-            .then ( ( value ) =>  { 
-              document.getElementById("update-form-administrador").reset();
-              traer_administrador();
-            } ) ;
-          }
-          traer_administrador();
-        }  
-      });
-      return false;
-    }); 
-</script>
+
